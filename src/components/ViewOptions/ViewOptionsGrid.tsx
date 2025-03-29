bd10609177f3b58621027a6113d8a4c81ff31329
Martyna Runkowska <martyna@cosy.land>
2025-03-29 03:06:27 +0000

    add: static stand-in for sort command.

--- src/components/ViewOptions/ViewOptionsGrid.tsx
+++ src/components/ViewOptions/ViewOptionsGrid.tsx
@@ -1,12 +1,13 @@
+import { css } from '../../../styled-system/css'
 import pin from '../../commands/pin'
 import pinAll from '../../commands/pinAll'
 import proseView from '../../commands/proseView'
-import subcategorizeOne from '../../commands/subcategorizeOne'
 import toggleDone from '../../commands/toggleDone'
 import toggleTableView from '../../commands/toggleTableView'
 import PanelCommand from '../CommandMenu/PanelCommand'
 import PanelCommandGroup from '../CommandMenu/PanelCommandGroup'
 import PanelGrid from '../SwipeablePanel/PanelGrid'
+import SortIcon from '../icons/SortIcon'
 
 /**
  * A grid of commands for the View Options feature.
@@ -18,11 +19,39 @@ const ViewOptionsGrid = () => {
         <PanelCommand command={pin} size='small' />
         <PanelCommand command={pinAll} size='small' />
       </PanelCommandGroup>
-      <PanelCommand command={subcategorizeOne} size='medium' />
+      {/* Sort Command Stand-in */}
+      <div
+        className={css({
+          display: 'grid',
+          gridTemplateColumns: '1fr 2fr',
+          minHeight: '3rem',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontWeight: 'bold',
+          padding: '0.5rem',
+          borderRadius: '16px',
+          backgroundColor: 'purple',
+          gridColumn: 'span 2',
+        })}
+      >
+        <SortIcon />
+        <div
+          className={css({
+            fontSize: 'md',
+            marginTop: '0',
+            color: 'fg',
+            textAlign: 'left',
+          })}
+        >
+          Sort
+          <br />
+          <div className={css({ fontWeight: 'normal' })}>Descending</div>
+        </div>
+      </div>
       <PanelCommandGroup>
-        <PanelCommand command={toggleDone} size='small' />
-        <PanelCommand command={proseView} size='small' />
-        <PanelCommand command={toggleTableView} size='small' />
+        <PanelCommand command={{ ...toggleDone, label: 'Done' }} size='small' />
+        <PanelCommand command={{ ...proseView, label: 'Prose' }} size='small' />
+        <PanelCommand command={{ ...toggleTableView, label: 'Table' }} size='small' />
       </PanelCommandGroup>
     </PanelGrid>
   )
---
+++ src/components/icons/SortIcon.tsx
@@ -0,0 +1,27 @@
+import { token } from '../../../styled-system/tokens'
+import { css } from '../../../styled-system/css'
+
+/**
+ * A sort icon.
+ */
+const SortIcon = ({ size = 30 }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={size}
+    height={size}
+    fill='none'
+    viewBox='0 0 24 24'
+    className={css({ cursor: 'pointer', pointerEvents: 'all', justifySelf: 'center' })}
+  >
+    <g stroke={token('colors.fg')} strokeLinecap='round' strokeLinejoin='round' clipPath='url(#clip0_856_2432)'>
+      <path d='M5.51 4.33v15.34M2.27 16.42l3.24 3.24 3.3-3.3M12.08 4.73h3.12M12.08 9.71h5.7M12.08 14.69h7.4M12.08 19.67h9.65'></path>
+    </g>
+    <defs>
+      <clipPath id='clip0_856_2432'>
+        <path fill={token('colors.fg')} d='M0 0h24v24H0z'></path>
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export default SortIcon

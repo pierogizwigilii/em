81fd9922c836d636b65616549483d8b2b079b6f6
Martyna Runkowska <martyna@cosy.land>
2025-03-29 07:12:12 +0000

    chore: made sure to use the fill prop.

--- src/components/icons/OutlineViewIcon.tsx
+++ src/components/icons/OutlineViewIcon.tsx
@@ -9,11 +9,11 @@ const OutlineViewIcon = ({ size = 18, fill = token('colors.fg') }) => (
     fill='none'
     viewBox='0 0 23 20'
   >
-    <circle cx='8' cy='11' r='2' fill={token('colors.fg')}></circle>
-    <circle cx='8' cy='18' r='2' fill={token('colors.fg')}></circle>
-    <path stroke='#fff' strokeLinecap='round' strokeWidth='2' d='M9 4h13M13 11h9M13 18h9'></path>
+    <circle cx='8' cy='11' r='2' fill={fill}></circle>
+    <circle cx='8' cy='18' r='2' fill={fill}></circle>
+    <path stroke={fill} strokeLinecap='round' strokeWidth='2' d='M9 4h13M13 11h9M13 18h9'></path>
     <path
-      fill={token('colors.fg')}
+      fill={fill}
       d='M4.15 5.875a.75.75 0 0 1-1.3 0l-1.732-3a.75.75 0 0 1 .65-1.125h3.464a.75.75 0 0 1 .65 1.125z'
     ></path>
   </svg>
